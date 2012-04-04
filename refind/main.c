b98fc0354a85252bc567e03b3f4f0b5625729ba7
srs5694 <srs5694@users.sourceforge.net>
2012-04-04 12:18:56 -0400

    Removed stray file.

--- refind/main.c
+++ refind/main.c
@@ -1141,6 +1141,28 @@ static VOID ScanDriverDir(IN CHAR16 *Path)
         CheckError(Status, FileName);
     }
 }
+EFI_STATUS
+LibScanHandleDatabase (
+     EFI_HANDLE  DriverBindingHandle, OPTIONAL
+     UINT32      *DriverBindingHandleIndex, OPTIONAL
+     EFI_HANDLE  ControllerHandle, OPTIONAL
+     UINT32      *ControllerHandleIndex, OPTIONAL
+     UINTN       *HandleCount,
+     EFI_HANDLE  **HandleBuffer,
+     UINT32      **HandleType
+     );
+#define EFI_HANDLE_TYPE_UNKNOWN                     0x000
+#define EFI_HANDLE_TYPE_IMAGE_HANDLE                0x001
+#define EFI_HANDLE_TYPE_DRIVER_BINDING_HANDLE       0x002
+#define EFI_HANDLE_TYPE_DEVICE_DRIVER               0x004
+#define EFI_HANDLE_TYPE_BUS_DRIVER                  0x008
+#define EFI_HANDLE_TYPE_DRIVER_CONFIGURATION_HANDLE 0x010
+#define EFI_HANDLE_TYPE_DRIVER_DIAGNOSTICS_HANDLE   0x020
+#define EFI_HANDLE_TYPE_COMPONENT_NAME_HANDLE       0x040
+#define EFI_HANDLE_TYPE_DEVICE_HANDLE               0x080
+#define EFI_HANDLE_TYPE_PARENT_HANDLE               0x100
+#define EFI_HANDLE_TYPE_CONTROLLER_HANDLE           0x200
+#define EFI_HANDLE_TYPE_CHILD_HANDLE                0x400
 
 static EFI_STATUS ConnectAllDriversToAllControllers(VOID)
 {
